f41cbe0411e721576282ef5961a6d11b5d2b96ee
Mihai Banulescu <mihai.banulescu@gmail.com>
2020-04-16 04:27:40 -0700

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'debug'
+  end
+
+
   namespace :api do
     namespace :v1 do
       resources :players do
-        resources :aktions, only: [:index, :show]        
+        resources :aktions, only: [:index, :show]
       end
     end
   end
