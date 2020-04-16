fbe773165a102efe32354dde8e7399668410dc47
Mihai Banulescu <mihai.banulescu@gmail.com>
2020-04-16 03:27:15 -0700

    add tagging

--- app/helpers/aktions_helper.rb
+++ app/helpers/aktions_helper.rb
@@ -1,2 +1,7 @@
 module AktionsHelper
+  def is_tagging(action, default='flowing')
+    return "is #{default}" unless action&.verb&.name
+
+    "#{action.player.first_name} is #{action&.verb&.name&.downcase}"
+  end
 end
